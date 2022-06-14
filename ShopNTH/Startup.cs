1d344b423a2dfe577f3b6bf3a7c7e30f533cb786
Ngân Bùi <btkimngan0807@gmail.com>
2022-06-14 20:17:53 +0700

    Add project files.

---
+++ ShopNTH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopNTH.Startup))]
+namespace ShopNTH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
