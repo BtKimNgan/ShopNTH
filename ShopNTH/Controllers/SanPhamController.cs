1d4f2b6b036efd0c1902451111da2f11b6455808
Ngân Bùi <btkimngan0807@gmail.com>
2022-06-15 21:47:01 +0700

    Upload lần 1

---
+++ ShopNTH/Controllers/SanPhamController.cs
@@ -0,0 +1,21 @@
+﻿using ShopNTH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopNTH.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        
+        // GET: SanPham
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult ListSanPham()
+        {
+            var all_SanPham = from ss in data.SanPhams select ss;
+            return View(all_SanPham);
+        }
+    }
+}
